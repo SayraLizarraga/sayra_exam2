e092f927746e669b7cece13bddb7a15201725474
SayraLizarraga <126122923+SayraLizarraga@users.noreply.github.com>
2023-03-22 18:38:13 -0600

    2

--- app/Http/Controllers/table_pokemonController.php
+++ app/Http/Controllers/table_pokemonController.php
@@ -8,7 +8,7 @@ use App\Models\table_pokemon;
 class table_pokemonController extends Controller
 {
     public function index (){
-        $page_title = "Super heroes";
+        $page_title = "Pokemons";
         $pokemon = table_pokemon::all();
         return view ('pokemon', compact('page_title', 'pokemon'));
        }
